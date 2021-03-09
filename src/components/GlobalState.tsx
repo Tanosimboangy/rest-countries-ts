d9c5c299011bbabd9649ebf80afb88a0e9eda11b
Tanosimboangy <jacquit.aja@onja.org>
2021-03-09 12:04:04 +0300

    fetching the data

--- src/components/GlobalState.tsx
+++ src/components/GlobalState.tsx
@@ -1,49 +1,74 @@
-import React, {useReducer, createContext } from 'react';
+import React, {useEffect, useReducer, createContext } from 'react';
 const URLALL = "https://restcountries.eu/rest/v2/all";
-// const [data, setData] = useState([]);
 
-// useEffect(() => {
-//     // const fetch = fetch(URLALL);
-// }, [])
 
-// function fetchingData() {
-    
-// }
+export const initialValue: State = {
+    countriesData: [],
+}
 
 type State = {
-    rValue: boolean;
+    countriesData: any,
 }
 
-export const initialValue = {
-    rValue: true,
-    turnOn: () => {},
+type data = {
+    alpha2Code: string
+    alpha3Code: string
+    altSpellings: [string, string]
+    area: number
+    borders:  [string, string, string, string, string, string,]
+    callingCodes: [string]
+    capital: string
+    cioc: string
+    currencies: [object[]]
+    demonym: string
+    flag: string
+    gini: number
+    languages: [object[], object[], object[]]
+    latlng: [number, number]
+    name: string
+    nativeName: string
+    numericCode: string
+    population: number
+    region: string
+    regionalBlocs: [object[]]
+    subregion: string
+    timezones: [string]
+    topLevelDomain: [string]
+    translations: {de: string, es: string, fr: string, ja: string, it: string}
+    __proto__: Object[]
 }
-
 
 type Action = 
-| {type: "one"} 
+|   {type: "FETCHING_COUNTRIES_DATA", payload: data[]}
+
 
 const GlobalContext = createContext(initialValue);
 export default GlobalContext;
 
-function reducer(state: State, action: Action) {
+function reducer(state: State = initialValue, action: Action) {
     switch(action.type) {
-        case "one":
-            return {rValue: true};
-            default:
-                return state;
+        case "FETCHING_COUNTRIES_DATA":
+            return {...state, countriesData: action.payload};
+        default:
+            return state;
     }
 }
 
 export const GlobalProvider: React.FC = ({children}) => {
     const [state, dispatch] = useReducer(reducer, initialValue);
+    
+    async function gettingCountriesData() {
+        const FetchingCountries = await fetch(URLALL);
+        const res = await FetchingCountries.json();        
+        dispatch({ type: 'FETCHING_COUNTRIES_DATA', payload: res})
+    }
+    
+    useEffect(() => {
+        gettingCountriesData();
+    }, [])
 
     return (
-        <GlobalContext.Provider 
-            value={{
-                rValue: state.rValue, 
-                turnOn: () => dispatch({type: "one"})
-                }}>
+        <GlobalContext.Provider value={{countriesData: state.countriesData}}>
             {children}
         </GlobalContext.Provider>
         )
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import GlobalContext from './GlobalState';
 
 function Home() {
+    const {countriesData} = useContext(GlobalContext);
+    console.log(countriesData);
+    
     return (
-        <h2>You are at home jacquit</h2>
+        <>
+            {
+                countriesData.map((coutry: any) => {
+                    return (
+                        <div>{coutry.name}</div>
+                    )
+                })
+            }
+        </>
     )
 }
 export default Home;
